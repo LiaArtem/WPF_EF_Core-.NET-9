06b9737794dd1334899a5f3f777dc4cd5c2cd3c6
Artem <metrix@live.ru>
2020-04-18 16:50:16 +0300

    Добавьте файлы проекта.

---
+++ AddWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_EF_Core
+{
+    /// <summary>
+    /// Логика взаимодействия для AddWindow.xaml
+    /// </summary>
+    public partial class AddWindow : Window
+    {
+        public AddWindow()
+        {
+            InitializeComponent();
+
+            // подписываем textBox на событие PreviewTextInput, с помощью которого можно обрабатывать вводимый текст
+            col_int.PreviewTextInput += new TextCompositionEventHandler(TextBox_PreviewTextInput_Int);
+            col_num.PreviewTextInput += new TextCompositionEventHandler(TextBox_PreviewTextInput_Float);
+            col_double.PreviewTextInput += new TextCompositionEventHandler(TextBox_PreviewTextInput_Float);
+        }
+
+        // Сохранить
+        private void Button_saveClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        // Отменить
+        private void Button_cancelClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void TextBox_PreviewTextInput_Float(object sender, TextCompositionEventArgs e)
+        {
+            string inputSymbol = e.Text.ToString(); // можно вводить цифры и точку
+            if (!Regex.Match(inputSymbol, @"[0-9]|\.").Success)
+            {
+                e.Handled = true;
+            }
+        }
+        private void TextBox_PreviewTextInput_Int(object sender, TextCompositionEventArgs e)
+        {
+            string inputSymbol = e.Text.ToString(); // можно вводить цифры
+            if (!Regex.Match(inputSymbol, @"[0-9]").Success)
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_EF_Core
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // изменение типа базы данных
+        private void Database_type_SelectedIndexChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox comboBox = (ComboBox)sender;
+            ComboBoxItem selectedItem = (ComboBoxItem)comboBox.SelectedItem;
+            String database_type = selectedItem.Content.ToString();
+        }
+
+        // добавить запись
+        private void Button_insertClick(object sender, RoutedEventArgs e)
+        {
+            AddWindow ww = new AddWindow();
+            ww.Show();
+        }
+
+        // изменить запись
+        private void Button_updateClick(object sender, RoutedEventArgs e)
+        {
+        }
+
+        // удалить запись
+        private void Button_deleteClick(object sender, RoutedEventArgs e)
+        {
+        }
+
+        // обновить запись
+        private void Button_selectClick(object sender, RoutedEventArgs e)
+        {
+        }
+
+    }
+}
