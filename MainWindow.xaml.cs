fe4bc0676b22d7cf2306e1f9152143c43e85dfcf
Artem Liakhovskyi <54625091+LiaArtem@users.noreply.github.com>
2023-08-13 11:19:35 +0300

    update nuget package

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -475,10 +475,10 @@ namespace WPF_EF_Core
             String database_type = selectedItem.Content.ToString();
             ConnectionStringGlobal = "";
 
-            // пока не поддерживает .net 7.0            
+            // пока не поддерживает .net            
             if (database_type == "IBM Informix" || database_type == "Firebird")
             {
-                MessageBox("Пока не поддерживает .NET 7.0", System.Windows.MessageBoxImage.Error);
+                MessageBox("Пока не поддерживает текущую версию .NET, макс. версия .NET 6.0", System.Windows.MessageBoxImage.Error);
                 DataGrid1.ItemsSource = null;
                 return;
             }            
